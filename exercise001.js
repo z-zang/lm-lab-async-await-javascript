28f0390b7be3593514fe1b01b94cac2a50bf3e9d
Zichao Zang <zang.code@gmail.com>
2023-06-05 19:39:06 +0100

    Finish exercises 1 & 2

--- exercise001.js
+++ exercise001.js
@@ -2,6 +2,7 @@ const promise = new Promise((resolve, reject) => {
 	// Generate random number between 0 and 9
 	const randomInt = Math.floor(Math.random() * 10);
 
+	// console.log(`randomInt: ${randomInt}`) // used this to check answer
 	if (randomInt % 2 === 0) {
 		// Success
 		setTimeout(() => resolve('done'), 2000);
@@ -12,3 +13,9 @@ const promise = new Promise((resolve, reject) => {
 });
 
 // Your solution(s) to exercise001 here!
+promise.then((resolve) => {
+	console.log(`Yay! Promise resolved with response: ${resolve}`)
+},
+(reject) => {
+	console.log(`Boo. Promise rejected with response:  ${reject}`)
+})
--- exercise002.js
+++ exercise002.js
@@ -1,6 +1,8 @@
 import fetch from "node-fetch";
 
 const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
+const jsonTypicodeError = "https://falseurl/throw-error";
+
 
 const fetchData = (apiEndPoint) => {
   fetch(apiEndPoint)
@@ -9,4 +11,19 @@ const fetchData = (apiEndPoint) => {
     .catch((error) => console.log(error));
 };
 
-fetchData(jsonTypicode);
+// fetchData(jsonTypicode);
+
+
+const fetchDataAsync = async (apiEndPoint) => {
+  try {
+    const response = await fetch(apiEndPoint);
+    const responseJson = await response.json();
+    console.log(responseJson)
+
+  } catch (error) {
+    console.log(`Error: ${error}`)
+  }
+}
+
+fetchDataAsync(jsonTypicode);
+fetchDataAsync(jsonTypicodeError); // used to check error handling
